043e7c4506dc2a8ff5053e5f9f31202b3937f954
dorian fiolet <dorian3012@hotmail.fr>
2025-01-08 03:12:01 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Item.destroy_all
+
+puts "Creating 10 items..."
+
+10.times do |i|
+  item = Item.create!(
+    name: "Item ##{i + 1}",
+    description: "Description for Item ##{i + 1}",
+    price: rand(10.0..500.0).round(2) # Prix aléatoire entre 10€ et 500€
+  )
+  puts "Created: #{item.name} - #{item.price}€"
+end
+
+puts "✅ Seeding completed!"
