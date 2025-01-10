e92554ae6d9a61c32f323ede4e55f7a0f84ff25f
dorian fiolet <dorian3012@hotmail.fr>
2025-01-10 05:17:45 +0100

    crud item done

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "font", to: "pages#font"
+  get 'dashboard', to: 'pages#dashboard'
+
+  resources :items
 
 end
